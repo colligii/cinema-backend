c2b55d97438328568e84187e4e2461c4d374a935
Giovanne Colli <cgiovanne45@gmail.com>
2026-01-04 18:27:14 -0300

    add cast module

--- src/cast/cast.module.ts
+++ src/cast/cast.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MovieCast } from './entity/movie_cast.entity';
+import { Cast } from './entity/cast.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([Cast, MovieCast])],
+})
 export class CastModule {}
